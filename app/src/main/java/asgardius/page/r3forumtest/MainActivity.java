a3c2324d6a83d9581d41b0c4ff8307493f8aa286
Page Asgardius <asgardius@asgardius.company>
2022-09-13 19:02:30 -0700

    login form

--- app/src/main/java/asgardius/page/r3forumtest/MainActivity.java
+++ app/src/main/java/asgardius/page/r3forumtest/MainActivity.java
@@ -2,7 +2,10 @@ package asgardius.page.r3forumtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button signUp = (Button)findViewById(R.id.signup);
+        signUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //buttonaction
+                SignUpForm();
+            }
+        });
+    }
+
+    private void SignUpForm () {
+
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/asgardius/page/r3forumtest/SignUp.java
@@ -0,0 +1,28 @@
+package asgardius.page.r3forumtest;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import asgardius.page.r3forumtest.databinding.ActivitySignUpBinding;
+
+public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
